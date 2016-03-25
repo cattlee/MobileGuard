1e37087f5695f2578484bafe2f046420c9951878
cattlee <305851097@qq.com>
2016-03-25 10:55:26 +0800

    实现sim卡  更换发送报警信息功能
    由于需要在手机重启时 判断sim卡 是否更换  则利用广播接收器（broadcast）BootReceiver 完成 在清单文件中设置接收器的
    <intent-filter>
    <action android:name="android.intent.action.BOOT_COMPLETED"></action>
    </intent-filter>即已完成重启就开始运行
    通过telephoneManager获取sim卡 序号从而判断

---
+++ src/com/itheima62/mobileguard/receiver/BootReceiver.java
@@ -0,0 +1,46 @@
+package com.itheima62.mobileguard.receiver;
+
+import com.itheima62.mobileguard.utils.MyConstants;
+import com.itheima62.mobileguard.utils.SpTools;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.SmsManager;
+import android.telephony.gsm.SmsMessage;
+
+/*
+ * 开机启动的广播接收者
+ * 本广播接收者通过清单文件进行注册
+ * 由于需要加载广播接收者需要开机  就执行  需加载权限   android.permission.RECEIVE_BOOT_COMPLETED
+ * */
+public class BootReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// 手机启动完成，检测sim卡 是否变化
+		//取出保存的sim卡信息  
+		//SpTools.getString(context, MyConstants.SIM, "")第二个参数为key  第三个参数为默认值
+		String oldSim = SpTools.getString(context, MyConstants.SIM, "");
+		//获取当前sim卡信息
+		TelephonyManager tm =(TelephonyManager) context.getSystemService(context.TELEPHONY_SERVICE);
+		//getSimSerialNumber()获取sim卡序列号
+		String simSerialNumber=tm.getSimSerialNumber();
+		
+		//判断sim卡是否发生变化
+		//为了测试 故意更改sim卡 序列号
+		if(!oldSim.equals(simSerialNumber+"1")){
+			//sim卡发生变化 发送报警短信
+			//取出安全号码,号码肯定有
+			String safeNumber=SpTools.getString(context, MyConstants.SAFENUMBER,"");
+			//发送短信个安全号码,添加发送权限   android.permission.SEND_SMS
+			SmsManager sm=SmsManager.getDefault();
+			sm.sendTextMessage(safeNumber, "", "wo shi xiao tou", null, null);
+		}
+		
+
+	}
+	
+
+}
