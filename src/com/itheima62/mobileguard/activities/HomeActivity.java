58c5153f07dce9ff7fc345153a555981810dd190
cattlee <305851097@qq.com>
2016-03-29 18:39:52 +0800

    完成 在手机防盗界面更改  手机防盗 图标 名

--- src/com/itheima62/mobileguard/activities/HomeActivity.java
+++ src/com/itheima62/mobileguard/activities/HomeActivity.java
@@ -1,11 +1,6 @@
 package com.itheima62.mobileguard.activities;
 
 
-import com.itheima62.mobileguard.R;
-import com.itheima62.mobileguard.utils.Md5Utils;
-import com.itheima62.mobileguard.utils.MyConstants;
-import com.itheima62.mobileguard.utils.SpTools;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
@@ -14,6 +9,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
@@ -24,12 +20,18 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.itheima62.mobileguard.R;
+import com.itheima62.mobileguard.utils.Md5Utils;
+import com.itheima62.mobileguard.utils.MyConstants;
+import com.itheima62.mobileguard.utils.SpTools;
+
 /**
  * @author Administrator
  * 主界面
  */
 public class HomeActivity extends Activity {
 	private GridView gv_menus;//主界面的按钮
+	
 	private int icons[] = {R.drawable.safe,R.drawable.callmsgsafe,R.drawable.item_gv_selector_app
 			,R.drawable.taskmanager,R.drawable.netmanager,R.drawable.trojan
 			,R.drawable.sysoptimize,R.drawable.atools,R.drawable.settings};
@@ -40,114 +42,112 @@ public class HomeActivity extends Activity {
 
 	private AlertDialog dialog;
 
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		//
+		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		initView();//初始化界面
 		initData();//给GridView设置数据
 		initEvent();//初始化事件
 	}
-	
-	
-	 /**
-		 * 初始化组件的事件
-		 */
-		private void initEvent() {
-			gv_menus.setOnItemClickListener(new OnItemClickListener() {
 
-				@Override
-				public void onItemClick(AdapterView<?> parent, View view,
-						int position, long id) {
-					//判断点击位置
-					switch (position) {
-					case 0://手机防盗
-						//判断是否设置过密码，没有设置密码则弹出设置密码对话框，若已经设置过密码，则弹出登陆密码对话框
-						//从sp中去对应password密码信息  判断Myconstants 中是否已经有密码
-						if(TextUtils.isEmpty(SpTools.getString(getApplicationContext(), MyConstants.PASSWORD, ""))){
-							//没有设置过密码
-							//设置密码对话框
-							showSettingPassDialog();
-						}else{
-							//已经设置过密码
-							//进入登录对话框
-							showEnterPassDialog();
-						}
-					
-						break;
+	/**
+	 * 初始化组件的事件
+	 */
+	private void initEvent() {
+		gv_menus.setOnItemClickListener(new OnItemClickListener() {
 
-					default:
-						break;
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				//判断点击位置
+				switch (position) {
+				case 8: //设置中心
+					//打开设置中心的界面
+					Intent setting = new Intent(HomeActivity.this,SettingCenterActivity.class);
+					startActivity(setting);
+					break;
+				case 0://手机防盗
+					//自定义对话框
+					//是否设置过密码，没有设置密码，弹出设置密码的对话框，如果设置密码，登陆的对话框
+					//从sp中去对应password的密码信息
+					if (TextUtils.isEmpty(SpTools.getString(getApplicationContext(), MyConstants.PASSWORD, ""))){
+						//没有设置过密码
+						//设置密码对话框
+						showSettingPassDialog();
+					} else {//设置过密码
+						//输入密码的对话框
+						showEnterPassDialog();
 					}
+					break;
+
+				default:
+					break;
 				}
-
-			});
-		}
-
-
-/*
- * 显示自定义登录密码的对话框
- * 
- * */
-		 protected void showEnterPassDialog() {
-				AlertDialog.Builder builder = new AlertDialog.Builder(this);
-				View view = View.inflate(getApplicationContext(), R.layout.dialog_enter_password, null);
-				final EditText et_passone = (EditText) view.findViewById(R.id.et_dialog_enter_password_passone);
-				
-				Button bt_setpass = (Button) view.findViewById(R.id.bt_dialog_enter_password_login);
-				Button bt_cancel = (Button) view.findViewById(R.id.bt_dialog_enter_password_cancel);
-				
-				
-				builder.setView(view );
-				bt_setpass.setOnClickListener(new OnClickListener() {
-					
-					@Override
-					public void onClick(View v) {
-						//设置密码
-						String passone = et_passone.getText().toString().trim();
-						
-						if ( TextUtils.isEmpty(passone)){
-							Toast.makeText(getApplicationContext(), "密码不能为空", 1).show();
-							return;
-						}  else {
-							// 密码判断,md5 2次加密
-							passone = Md5Utils.md5(Md5Utils.md5(passone));
-							//读取sp中保存的密文，进行判断
-							if (passone.equals(SpTools.getString(getApplicationContext(), MyConstants.PASSWORD, ""))){
-								//一致
-								//进入手机防盗界面
-								Intent intent = new Intent(HomeActivity.this,LostFindActivity.class);
-								startActivity(intent);
-							} else {
-								//不一致
-								Toast.makeText(getApplicationContext(), "密码不正确", 1).show();
-								return;
-							}
-							
-							//关闭对话框
-							dialog.dismiss();
-						}
-						
-					}
-				});
-				bt_cancel.setOnClickListener(new OnClickListener() {
-					
-					@Override
-					public void onClick(View v) {
-						// TODO Auto-generated method stub
-						dialog.dismiss();//关闭对话框
-					}
-				});
-				dialog = builder.create();
-				dialog.show();
 			}
 
+		});
+	}
 
+	/**
+	 * 显示自定义输入密码的对话框
+	 */
+	protected void showEnterPassDialog() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		View view = View.inflate(getApplicationContext(), R.layout.dialog_enter_password, null);
+		final EditText et_passone = (EditText) view.findViewById(R.id.et_dialog_enter_password_passone);
+		
+		Button bt_setpass = (Button) view.findViewById(R.id.bt_dialog_enter_password_login);
+		Button bt_cancel = (Button) view.findViewById(R.id.bt_dialog_enter_password_cancel);
+		
+		
+		builder.setView(view );
+		bt_setpass.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//设置密码
+				String passone = et_passone.getText().toString().trim();
+				
+				if ( TextUtils.isEmpty(passone)){
+					Toast.makeText(getApplicationContext(), "密码不能为空", 1).show();
+					return;
+				}  else {
+					// 密码判断,md5 2次加密
+					passone = Md5Utils.md5(Md5Utils.md5(passone));
+					//读取sp中保存的密文，进行判断
+					if (passone.equals(SpTools.getString(getApplicationContext(), MyConstants.PASSWORD, ""))){
+						//一致
+						//进入手机防盗界面
+						Intent intent = new Intent(HomeActivity.this,LostFindActivity.class);
+						startActivity(intent);
+					} else {
+						//不一致
+						Toast.makeText(getApplicationContext(), "密码不正确", 1).show();
+						return;
+					}
+					
+					//关闭对话框
+					dialog.dismiss();
+				}
+				
+			}
+		});
+		bt_cancel.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				dialog.dismiss();//关闭对话框
+			}
+		});
+		dialog = builder.create();
+		dialog.show();
+	}
 
 	protected void showSettingPassDialog() {
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);//设置自定义对话框
-		View view = View.inflate(getApplicationContext(), R.layout.dialog_setting_password, null);//将自定义的布局转化为view
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		View view = View.inflate(getApplicationContext(), R.layout.dialog_setting_password, null);
 		final EditText et_passone = (EditText) view.findViewById(R.id.et_dialog_setting_password_passone);
 		final EditText et_passtwo = (EditText) view.findViewById(R.id.et_dialog_setting_password_passtwo);
 		Button bt_setpass = (Button) view.findViewById(R.id.bt_dialog_setting_password_setpass);
@@ -160,7 +160,7 @@ public class HomeActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				//设置密码
-				String passone = et_passone.getText().toString().trim();//。trim()去掉两侧空格
+				String passone = et_passone.getText().toString().trim();
 				String passtwo = et_passtwo.getText().toString().trim();
 				if (TextUtils.isEmpty(passtwo) || TextUtils.isEmpty(passone)){
 					Toast.makeText(getApplicationContext(), "密码不能为空", 1).show();
@@ -172,9 +172,8 @@ public class HomeActivity extends Activity {
 					//保存密码
 					//保存密码到sp中
 					 System.out.println("保存密码");
-					 //对密码加密  MD5加密两次   保存MD5加密后的密码passone  银行MD5加密十次以上 所以很难破解
-					 passone=Md5Utils.md5(Md5Utils.md5(passone));
-					 //保存密码到sp
+					 //对密码加密处理 ，md5 2次加密 保存密文
+					 passone = Md5Utils.md5(Md5Utils.md5(passone));
 					 SpTools.putString(getApplicationContext(), MyConstants.PASSWORD, passone);
 					 dialog.dismiss();
 				}
@@ -186,28 +185,62 @@ public class HomeActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				dialog.dismiss();//关闭对话框  builder没有dimiss功能 故使用dialog完成dimiss功能
+				dialog.dismiss();//关闭对话框
 			}
 		});
 		dialog = builder.create();
 		dialog.show();
 	}
 
-	/*
- * 初始化组件数据
- * */
+	/**
+	 * 初始化组件的数据
+	 */
 	private void initData() {
 		adapter = new MyAdapter();
 		gv_menus.setAdapter(adapter);//设置gridview适配器数据
+		
 	}
-	public class MyAdapter extends BaseAdapter{
+	
+	@Override
+	protected void onResume() {
+		//通知Gridview重新取数据
+		adapter.notifyDataSetChanged();
+		super.onResume();
+	}
+	
+	private class MyAdapter extends BaseAdapter{
 
 		@Override
 		public int getCount() {
-			// 指定该adapter包含多少选项
-			return icons.length;//数组长度（图标个数）
+			// TODO Auto-generated method stub
+			return icons.length;//图标的个数
+		}
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View view = View.inflate(getApplicationContext(), R.layout.item_home_gridview, null);
+			//获取组件
+			ImageView iv_icon = (ImageView) view.findViewById(R.id.iv_item_home_gv_icon);
+			TextView tv_name = (TextView) view.findViewById(R.id.tv_item_home_gv_name);
+			//设置数据
+			//图片
+			iv_icon.setImageResource(icons[position]);
+			
+			//文字
+			tv_name.setText(names[position]);
+			
+			if (position == 0) {//只判断手机防盗的位置
+				//判断是否存在新的手机防盗名
+				if (!TextUtils.isEmpty(SpTools.getString(getApplicationContext(), MyConstants.LOSTFINDNAME, ""))) {
+					//有新的手机防盗名
+					tv_name.setText(SpTools.getString(getApplicationContext(), MyConstants.LOSTFINDNAME, ""));
+				}
+			}
+			
+			
+			return view;
 		}
 
+		
 		@Override
 		public Object getItem(int position) {
 			// TODO Auto-generated method stub
@@ -216,32 +249,16 @@ public class HomeActivity extends Activity {
 
 		@Override
 		public long getItemId(int position) {
-			// 返回列表项的id 
+			// TODO Auto-generated method stub
 			return 0;
 		}
 
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			//该方法所返回的view 将作为列表项    
-			//第一个参数为上下文，第二个参数为第三个参数为列表项提供数据，这里先初始化为null  让后续通过iv_icon 和 tv_name 完成设置
-			View view = View.inflate(getApplicationContext(), R.layout.item_home_gridview, null);
-			//获取组件
-			ImageView iv_icon = (ImageView) view.findViewById(R.id.iv_item_home_gv_icon);
-			TextView tv_name = (TextView) view.findViewById(R.id.tv_item_home_gv_name);
-			//设置数据
-			//图片
-			iv_icon.setImageResource(icons[position]);
-			//文字
-			tv_name.setText(names[position]);
-			
-			return view;
-
-		}
+		
 		
 	}
 
 	private void initView() {
-		// 
+		// TODO Auto-generated method stub
 		setContentView(R.layout.activity_home);
 		gv_menus = (GridView) findViewById(R.id.gv_home_menus);
 	}
--- src/com/itheima62/mobileguard/activities/LostFindActivity.java
+++ src/com/itheima62/mobileguard/activities/LostFindActivity.java
@@ -5,11 +5,21 @@ import com.itheima62.mobileguard.utils.MyConstants;
 import com.itheima62.mobileguard.utils.SpTools;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LostFindActivity extends Activity {
+
+	private AlertDialog dialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,4 +53,80 @@ public class LostFindActivity extends Activity {
 		setContentView(R.layout.activity_lostfind);
 		
 	}
+	/*
+	 * 创建菜单的方法
+	 * */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	/*
+	 * 处理菜单事件
+	 * */
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+		case R.id.mn_modify_name:
+			Toast.makeText(getApplicationContext(), "修改菜单名", 1).show();
+			//弹出对话框，让用户输入新的手机防盗名
+			showModifyNameDialog();
+			break;
+		case R.id.mn_test_menu:
+			Toast.makeText(getApplicationContext(), "测试菜单", 1).show();
+		default:
+			break;
+		}
+		return super.onMenuItemSelected(featureId, item);
+	}
+
+/*
+ * 修改手机防盗名的对话框
+ * */
+	private void showModifyNameDialog() {
+		AlertDialog.Builder ab = new AlertDialog.Builder(this);
+		//对话框显示的界面
+		View view=View.inflate(getApplicationContext(),R.layout.dialog_modify_name,null);
+		ab.setView(view);//设置view
+		
+		//处理界面的事件
+		final EditText et_name=(EditText)view.findViewById(R.id.et_dialog_lostfind_modify_name);
+		Button bt_modify=(Button) view.findViewById(R.id.bt_dialog_lostfind_modify); 
+		Button bt_cancel=(Button) view.findViewById(R.id.bt_dialog_enter_password_cancel);
+		
+		//处理按钮事件
+		bt_cancel.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				dialog.dismiss();//对话框消失
+				
+			}
+		});
+		bt_modify.setOnClickListener(new OnClickListener(){
+			
+			@Override
+			public void onClick(View v) {
+				//获取修改的名字
+				String name =et_name.getText().toString().trim();
+				if(TextUtils.isEmpty(name)){
+				Toast.makeText(getApplicationContext(), "名字不可为空", 1).show();
+				return;
+				
+			}
+			//保存新名字到sp中
+			SpTools.putString(getApplicationContext(),MyConstants.LOSTFINDNAME,name);
+			dialog.dismiss();
+			Toast.makeText(getApplicationContext(), "名字修改成功", 1).show();
+			}
+		});
+		
+		dialog = ab.create();//创建对话框
+		dialog.show();
+
+	}
+
 }
