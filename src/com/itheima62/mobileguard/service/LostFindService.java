c61e9414e96f6bd754ec9964f6df9b0d20a8cd69
cattlee <305851097@qq.com>
2016-03-29 10:25:04 +0800

    完成远程发送短信   手机自动播放音乐功能
    调用mediaplayer      setVolume（）设置音量  start（）开始播放
    setoncompletionlistener(listener) 设置音乐播放完毕出发此方法

--- src/com/itheima62/mobileguard/service/LostFindService.java
+++ src/com/itheima62/mobileguard/service/LostFindService.java
@@ -6,63 +6,86 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.telephony.SmsMessage;
 
 public class LostFindService extends Service {
-	
+
 	private SmsReceiver receiver;
+	private boolean isPlay;//false 音乐播放的标记
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
-		
 		return null;
 	}
+	
 	/**
 	 * @author Administrator
-	 * 短信的广播接收者
+	 * ���ŵĹ㲥������
 	 */
 	private class SmsReceiver extends BroadcastReceiver{
 
 		@Override
 		public void onReceive(Context context, Intent intent) {
 			// TODO Auto-generated method stub
-			//实施短信拦截功能
-			
+			//实现短信拦截
 			Bundle extras = intent.getExtras();
+		    // int i = 3 
 			
+			 
 			Object datas[] = (Object[]) extras.get("pdus");
 			for (Object data:datas){
 				SmsMessage sm = SmsMessage.createFromPdu((byte[]) data);
 				//System.out.println(sm.getMessageBody() + ":" + sm.getOriginatingAddress());
-				String mess=sm.getMessageBody();//获取短信内容
-				if (mess.equals("#*gps*#")) {//由短信内容  判断需要获取定位信息
-					//手机需要定位一段时间才能获取定位信息，则 耗时操作应该服务中完成
+				String mess = sm.getMessageBody();//获取短信内容
+				if (mess.equals("#*gps*#")){//获取 去 定位的信息
+					//耗时的定位，把定位的功能防盗服务中执行
 					Intent service = new Intent(context,LocationService.class);
-					startService(service);//启动定位的服务
-					System.out.println("gps intent ok");
+					startService(service);//启动定位服务
+					
 					abortBroadcast();//终止广播
-					}
-				else if(mess.equals("#*lockscreen*#")){
-					//获得设备管理器
-					DevicePolicyManager dpm=(DevicePolicyManager)getSystemService(DEVICE_POLICY_SERVICE);
-					//加密设置密码
+				} else if (mess.equals("#*lockscreen*#")){
+					//获取设备管理器
+					DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
+					//重置锁屏密码
 					dpm.resetPassword("123", 0);
 					//一键锁屏
 					dpm.lockNow();
 					abortBroadcast();//终止广播
-				}
-				else if(mess.equals("#*wipedata*#")){
-					//远程清除数据
+				} else if (mess.equals("#*wipedata*#")){//远程擦除信息
 					//获取设备管理器
-					DevicePolicyManager dpm=(DevicePolicyManager)getSystemService(DEVICE_POLICY_SERVICE);
-					//清除sd卡数据
-					dpm.wipeData(0);
+					DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
+					//设置密码
+					dpm.wipeData(DevicePolicyManager.WIPE_EXTERNAL_STORAGE);
 					abortBroadcast();//终止广播
+				}  else if (mess.equals("#*music*#")){
+					//ֻ只播放一次
+					abortBroadcast();
+					if (isPlay) {
+						return;
+					}
+						
+					//播放音乐
+					MediaPlayer mp = MediaPlayer.create(getApplicationContext(), com.itheima62.mobileguard.R.raw.qqqg);
+					//设置音量值最大ֵ
+					mp.setVolume(1, 1);
+					mp.start();//开始播放
+					mp.setOnCompletionListener(new OnCompletionListener() {
+						
+						@Override
+						public void onCompletion(MediaPlayer mp) {
+							//播放完毕触发方法
+							isPlay = false;
+						}
+					});;
+					isPlay = true;
 					
 				}
-				}
+			}
 		}
 		
 	}
@@ -72,8 +95,8 @@ public class LostFindService extends Service {
 		//短信广播接收者
 		receiver = new SmsReceiver();
 		IntentFilter filter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
-		filter.setPriority(Integer.MAX_VALUE);//设置优先级   级别一样 清单文件谁先注册谁先执行，如果级别一样，代码比清单级别高
-		//注册短信监听
+		filter.setPriority(Integer.MAX_VALUE);//级别一样，清单文件，谁先注册先执行，级别一样，代码比清单要高
+		//注册手机监听
 		registerReceiver(receiver, filter );
 		super.onCreate();
 	}
@@ -81,7 +104,7 @@ public class LostFindService extends Service {
 	@Override
 	public void onDestroy() {
 		// TODO Auto-generated method stub
-		//
+		//取消注册手机监听
 		unregisterReceiver(receiver);
 		super.onDestroy();
 	}
